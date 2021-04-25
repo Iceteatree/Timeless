37659ad930a4919f3a43a03172a05565d98b3cb8
Alan <alankow11@gmail.com>
2021-04-25 18:01:22 +0200

    add config and mongoose models

---
+++ models/Item.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ItemSchema = new Schema ({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    img_url: {
+        type:String,
+        required: false
+    },
+    date_added: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+module.exports = Item = mongoose.model('item', ItemSchema);
--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const path = require('path');
+const config = require('config');
 const mongoose = require('mongoose');
 require('dotenv').config()
 
 let app = express();
+// const uri = `mongodb+srv://${process.env.MONGO_DB_USERNAME}:${process.env.MONGO_DB_PASSWORD}@${process.env.MONGO_DB_CLUSTER}/timelessDatabase`
+const dbURI = config.get('dbURI'); //This does the same as above but with less process.env
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
@@ -14,11 +17,39 @@ if (process.env.NODE_ENV === 'production') {
       {res.sendFile(path.resolve(__dirname, 'timeless-app-frontend', 'build', 'index.html'));
   });
 }
+
 app.get('/', (req, res) => {
     res.send("Server is working.")
 })
 
-
-app.listen(process.env.PORT, () => {
-    console.log("Listening on port " + process.env.PORT)    
+// Initialising Mongoose. 
+mongoose.Promise = global.Promise;
+// Connecting to the database
+mongoose.connect(dbURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 })
+// Error messages
+mongoose.connection.on('error', function(err) {
+	console.log('Connection to Mongo established.');
+    console.log('Could not connect to the database. Exiting now...');
+    process.exit();
+});
+// If link has been established message
+mongoose.connection.once('open', function() {
+    console.log("Successfully connected to the database");
+})
+
+// If server has a 404 error display this message.
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+  });
+
+app.listen(process.env.PORT || 8000, () => {
+    console.log("Listening on port " + (process.env.PORT || 8000))    
+})
+
+
+module.exports = app;
